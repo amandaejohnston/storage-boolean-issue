cdc008c3d3c41e0197e26a00e5f861273cf41fd8
indraraj <indraraj@infiniteit.biz>
2022-08-07 22:04:26 +0530

    chore: add reproduction

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,16 +7,13 @@ import { Component, AfterViewInit } from '@angular/core';
   styleUrls: ['./home.page.scss'],
 })
 export class HomePage {
-
   test = 1;
+
   onRadioValueChanged(event: any) {
     console.log('radio trigger');
   }
 
-
   onIncreaseValue(event: any) {
-    event.preventDefault();
-    event.stopPropagation();
     console.log('on increase value');
     this.test += 1;
   }
